f21474175e43bb79755e33fa8185569f3f94f1ed
Andrius Dalisanskis <andrius.dalisanskis@gmail.com>
2021-09-21 10:29:06 +0100

    First commit

---
+++ src/exercises/ConditionalLogic.java
@@ -0,0 +1,48 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class ConditionalLogic {
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args){
+
+    }
+    private static void preAndPostDifference(){
+
+    }
+    private static void booleanLogicShortCircuitOps(){
+
+    }
+    private static void booleanBitwiseOps(){
+
+    }
+    private static void compareStrings(){
+
+    }
+    private static boolean admitToFilm(int arg1, int arg2){
+        return true;
+    }
+    private static void ternaryOperator(){
+
+    }
+    private static void ifMonth(){
+
+    }
+    private static void ifGrade(){
+
+    }
+    private static void switchMathOperation(){
+
+    }
+    private static void switchEnumType(){
+
+    }
+    private static void ifTemperature(){
+
+    }
+    private static void switchDaysInMonth(){
+
+    }
+
+}
